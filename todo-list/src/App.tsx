990c807fbf1522f7df4e8b107d13942284ba7dc4
d0561910 <d0561910@mail.fcu.edu.tw>
2020-05-31 19:12:41 +0800

    push react-todo-list

---
+++ todo-list/src/App.tsx
@@ -0,0 +1,142 @@
+// Import dependencies
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+// Import components
+import TodoForm from "./components/todo-form";
+import TodoList from "./components/todo-list";
+// Import interfaces
+import { TodoInterface } from "./interfaces";
+// Import styles
+import "./styles/styles.css";
+
+// TodoListApp component
+const TodoListApp = () => {
+  const [todos, setTodos] = useState<TodoInterface[]>([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://us-central1-todo-firebase-1a591.cloudfunctions.net/webApi/api/v1/api/data"
+      )
+      .then((res) => {
+        let items = res.data.data;
+        let todoItem = [];
+        for (let item in items) {
+          todoItem.push({
+            id: items[item].id,
+            task: items[item].task,
+            done: items[item].done,
+          });
+        }
+        setTodos(todoItem);
+      });
+  }, []);
+
+  // Creating new todo item
+  function handleTodoCreate(todo: TodoInterface) {
+    // Prepare new todos state
+    const newTodosState: TodoInterface[] = [...todos];
+    // Update new todos state
+    newTodosState.push(todo);
+    // Update todos state
+    setTodos(newTodosState);
+    axios
+      .post(
+        "https://us-central1-todo-firebase-1a591.cloudfunctions.net/webApi/api/v1/api/create",
+        todo
+      )
+      .then((res) => {
+        console.log(res);
+      });
+  }
+
+  // Update existing todo item
+  function handleTodoUpdate(
+    event: React.ChangeEvent<HTMLInputElement>,
+    id: string
+  ) {
+    // Prepare new todos state
+    const newTodosState: TodoInterface[] = [...todos];
+    // Find correct todo item to update
+    newTodosState.find((todo: TodoInterface) => todo.id === id)!.task =
+      event.target.value;
+
+    axios
+      .post(
+        "https://us-central1-todo-firebase-1a591.cloudfunctions.net/webApi/api/v1/api/update",
+        { id, task: event.target.value }
+      )
+      .then((res) => {
+        // Update todos state
+        setTodos(newTodosState);
+      });
+  }
+
+  // Remove existing todo item
+  function handleTodoRemove(id: string) {
+    // Prepare new todos state
+    const newTodosState: TodoInterface[] = todos.filter(
+      (todo: TodoInterface) => todo.id !== id
+    );
+    axios
+      .post(
+        "https://us-central1-todo-firebase-1a591.cloudfunctions.net/webApi/api/v1/api/delete",
+        { id }
+      )
+      .then((res) => {
+        // console.log(res);
+        // Update todos state
+        setTodos(newTodosState);
+      });
+  }
+
+  // Check existing todo item as completed
+  function handleTodoComplete(id: string) {
+    // Copy current todos state
+    const newTodosState: TodoInterface[] = [...todos];
+    // Find the correct todo item and update its 'isCompleted' key
+    newTodosState.find(
+      (todo: TodoInterface) => todo.id === id
+    )!.done = !newTodosState.find((todo: TodoInterface) => todo.id === id)!
+      .done;
+
+    let status = !newTodosState.find((todo: TodoInterface) => todo.id === id)!
+      .done;
+    // console.log({ status });
+
+    axios
+      .post(
+        "https://us-central1-todo-firebase-1a591.cloudfunctions.net/webApi/api/v1/api/status",
+        { id, done: !status }
+      )
+      .then((res) => {
+        console.log({res});
+        // Update todos state
+        setTodos(newTodosState);
+      });
+  }
+
+  // Check if todo item has title
+  function handleTodoBlur(event: React.ChangeEvent<HTMLInputElement>) {
+    if (event.target.value.length === 0) {
+      event.target.classList.add("todo-input-error");
+    } else {
+      event.target.classList.remove("todo-input-error");
+    }
+  }
+  return (
+    <div className="todo-list-app">
+      <TodoForm todos={todos} handleTodoCreate={handleTodoCreate} />
+      <TodoList
+        todos={todos}
+        handleTodoUpdate={handleTodoUpdate}
+        handleTodoRemove={handleTodoRemove}
+        handleTodoComplete={handleTodoComplete}
+        handleTodoBlur={handleTodoBlur}
+      />
+    </div>
+  );
+};
+
+export default TodoListApp;
